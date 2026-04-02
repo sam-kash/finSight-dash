5a43875487a88f06c9e699d470729ac80b4fb4b5
sam-kash <kashyapsankalpa@gmail.com>
2026-04-02 20:37:06 +0530

    infra tweaks

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import { errorHandler } from './middlewares/errorHandler'
 import authRouter from './modules/auth/auth.router'
+import usersRouter from './modules/users/users.router'
+import recordsRouter from './modules/records/records.router'
+import auditRouter from './modules/audit/audit.router'
 
 const app = express()
 
@@ -8,6 +11,9 @@ app.use(express.json())
 
 // Routes
 app.use('/api/auth', authRouter)
+app.use('api/users' , usersRouter)
+app.use('api/records', recordsRouter)
+app.use('api/audit', auditRouter)
 
 // Health check
 app.get('/health', (req, res) => {
